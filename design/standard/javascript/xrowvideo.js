c27582f96894943937057696f835f1c343ddc000
xinyuexrow <wang@xrow.de>
2014-03-04 16:55:00 +0100

    disable automatic Focus in web browsers

--- design/standard/javascript/xrowvideo.js
+++ design/standard/javascript/xrowvideo.js
@@ -1,6 +1,6 @@
 /* do: set player options */
 LBP.options = {
-    //focusFirstOnInit: true, // focus first (video) player on initialization
+    focusFirstOnInit: false, // focus first (video) player on initialization
     showSources: true, // if switch between available video qualities should be possible
     defaultTimerFormat: "PASSED_HOVER_REMAINING", // default timer format, could be "PASSED_DURATION" (default), "PASSED_REMAINING", "PASSED_HOVER_REMAINING"
     defaultLanguage: 'de',
