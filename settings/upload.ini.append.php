16cd7505f000a4db3ff597f52f2c385d97e013e3
xrow <bjoern@xrow.de>
2016-04-05 13:49:26 +0200

    - multiupload settings to prevent crash

---
+++ settings/upload.ini.append.php
@@ -0,0 +1,29 @@
+<?php /* #?ini charset="utf-8"?
+
+[CreateSettings]
+MimeClassMap[video/quicktime]=xrowvideo
+MimeClassMap[video/x-msvideo]=xrowvideo
+MimeClassMap[video/vnd.rn-realvideo]=xrowvideo
+MimeClassMap[application/vnd.rn-realmedia]=xrowvideo
+MimeClassMap[video/mp4]=xrowvideo
+MimeClassMap[video/3gpp]=xrowvideo
+MimeClassMap[video/x-ms-asf]=xrowvideo
+MimeClassMap[video/x-msvideo]=xrowvideo
+MimeClassMap[video/x-flv]=xrowvideo
+MimeClassMap[video/x-sgi-movie]=xrowvideo
+MimeClassMap[video/mp4]=xrowvideo
+MimeClassMap[video/mpeg]=xrowvideo
+MimeClassMap[video/mpeg]=xrowvideo
+MimeClassMap[video/mpeg]=xrowvideo
+MimeClassMap[video/ogg]=xrowvideo
+MimeClassMap[video/quicktime]=xrowvideo
+MimeClassMap[video/vnd.rn-realvideo]=xrowvideo
+MimeClassMap[video/webm]=xrowvideo
+MimeClassMap[video/x-ms-wmv]=xrowvideo
+
+[xrowvideo_ClassSettings]
+FileAttribute=file
+NameAttribute=name
+NamePattern=<original_filename_base>
+
+*/ ?>
