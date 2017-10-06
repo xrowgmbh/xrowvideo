d09ffa4a31795f6fc5bdecd5f41f8451ef4b9d55
Andy Caiger <andy@xrow.de>
2017-10-06 11:03:21 +0200

    Add Piwik tracking extension for LeanBack Player #9819

---
+++ design/standard/javascript/leanbackPlayer.ext.piwikTracking.pack.js
@@ -0,0 +1,21 @@
+/**
+ * Version: 0.1.0 - LeanBack Player Piwik Tracking Extension
+ * Date: 2011-12-28
+ *
+ * This Software is part of LeanBack Player [HTML5 Media Player UI] by Ronny Mennerich
+ * Copyright (c) 2010-2011 Ronny Mennerich <contact@leanbackplayer.com>
+ *
+ * LeanBack Player, in its open source version, is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * LeanBack Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LeanBack Player Extension. If not, see <http://www.leanbackplayer.com/license_gpl3.html/>.
+ */
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('3.2={8:{},I:"15,16,17,18,19,1a,1b".1c(",")};3.u.1d["q.2"]=!0;3.u.1e["q.2"]="k 1f";3.u.1g["q.2"]="x y";3.u.1h["q.2"]="C://1i.1j";3.u.1k["q.2"]="0.1.0 (1l 1.1m)";3.1n.1o("J");3.K.J=l(){9("7"===5 6.4.2)6.4.2={};9("7"===5 3.2.z)3.2.z=1p;3.L("1q",l(){6.4.2.m=6.i.r});3.L("1r",l(){6.4.2.m=6.i.r});9("7"!==5 3.2.8&&"7"!==5 3.2.8.M&&3.2.8.M&&"7"!==5 3.2.8.j&&"7"!==5 3.2.8.n){s a=3.2.8.j,a=-1!==a.D("/")&&a.D("/")===a.N-1?a:a+"/";3.2.8.j=-1!==a.D("C")?a:"O:"===P.1s.1t?"O://"+a:"C://"+a;s e=P,a=e.1u("Q"),e=e.1v("Q")[0];a.1w="1x/1y";a.1z=!0;a.1A=!0;a.A=3.2.8.j+"R.1B";e.1C.1D(a,e)}s h=l(a,b){a++;S.1E(3.2.z);"7"===5 3.2.8.n||1F<=a?"7"===5 3.2.8.n?3.E("k F G t x y T U t V. 1G 1H H 1I 1J 1K \'3.2.8.j\' 1L \'3.2.8.n\' H 1M q H 1N.","W"):3.E("k X T U t V. 1O 1P k F G t x y 1Q.","W"):"7"===5 k||"X"!==5 k?3.2.z=S.1R(l(){h(a,b)},20):b.Y()};h(0,6)};3.K.Y=l(){s a,e=3.2.I;9("7"===5 6.4.2.o)6.4.2.o=!1;9("7"===5 6.4.2.B)6.4.2.B=!1;9("7"===5 6.4.2.j&&"7"!==5 3.2.8.j)6.4.2.j=3.2.8.j;1S 9("7"===5 6.4.2.j&&"7"!==5 Z)6.4.2.j=Z;9("7"===5 6.4.2.n)6.4.2.n=3.2.8.n;9("7"===5 6.4.2.m)6.4.2.m=0;t(a=0;a<e.N;a++)(l(a,c,b){3.1T(b.i,c.p(),l(){s e,d,f;e=c.p();f="#"+c;d=!0;1U(e){v"o":b.4.2.m=b.i.r;d=!0;f="#"+c;w;v"11":d=3.12(b.4.2.m);f="#"+c+"="+d;w;v"13":b.4.2.m=b.i.r;d=3.12(b.i.r);f="#"+c+"="+d;w;v"1V":d=b.i.1W;f="#"+c+"="+d;w;v"1X":d=14(1Y*b.i.1Z,10);f="#"+c+"="+d;w;21:d=!0,f="#"+c}e=14(a+1,10);s g=k.22(b.4.2.j+"R.23",b.4.2.n);9("7"!==5 g){9("24"===c.p())b.4.2.B=!0,b.4.2.o=!1;9("o"===c.p())b.4.2.o=!0;"25"===c.p()&&b.4.2.o?b.4.2.o=!1:b.4.2.B&&!("11"===c.p()&&b.4.2.m>=b.i.26)&&!("13"===c.p()&&0.27>=b.i.r)&&(3.2.8.28&&3.E("k F G t x y 29 2a \'"+c+"\'"+("2b"===5 d?"":" (2c: \'"+d+"\')")+" 2d 2e 2f 2g A \'"+b.i.A+"\'","2h"),g.2i(b.i.A+f),g.2j(e,c,d.2k()),g.2l())}})})(a,e[a],6)};',62,146,'||piwikTracker|LBP|vars|typeof|this|undefined|options|if|||||||||html5Obj|piwikUrl|Piwik|function|storeCurrentTime|piwikSideID|ended|toLowerCase|extension|currentTime|var|for|Exts|case|break|LeanBack|Player|bufferproc|src|played|http|lastIndexOf|log|Tracker|Extension|to|trackEvents|initializePiwikTracker|prototype|preExtend|addJSCode|length|https|document|script|piwik|window|not|ready|tracking|warn|object|addPiwikTrackerEvents|pkBaseURL||seeking|parseTimer|seeked|parseInt|VolumeChange|RateChange|Seeking|Seeked|Ended|Play|Pause|split|Info|Name|Tracking|User|URL|leanbackplayer|com|Version|beta|20110419|initialize|push|null|onSeeking|seekTo|location|protocol|createElement|getElementsByTagName|type|text|javascript|defer|async|js|parentNode|insertBefore|clearTimeout|200|You|have|define|at|least|and|get|work|Setting|up|failed|setTimeout|else|addEvent|switch|ratechange|playbackRate|volumechange|100|volume||default|getTracker|php|play|pause|duration|01|debug|tracked|event|boolean|value|on|HTML5|media|element|info|setCustomUrl|setCustomVariable|toString|trackPageView'.split('|'),0,{}));
